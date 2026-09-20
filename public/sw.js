f10b9f828d0ec7601a0208da4b7490074e0c17de
shahrinahammad <shahrin.ahammad@shikho.com>
2026-09-20 15:58:43 +0600

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('push', function (event) {
+  if (event.data) {
+    const data = event.data.json()
+    const options = {
+      body: data.body,
+      icon: '/logo.png', // আপনার লোগো
+      badge: '/logo.png',
+      vibrate: [100, 50, 100],
+    }
+    event.waitUntil(self.registration.showNotification(data.title, options))
+  }
+})
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close()
+  event.waitUntil(clients.openWindow('/'))
+})
